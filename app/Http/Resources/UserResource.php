643b917f5ddf080645fc388c2aa9af2721a66baa
Carlos Ferreira <carlos@especializati.com.br>
2023-03-01 14:47:41 -0300

    refactor: format date to Y-m-d

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -18,7 +19,7 @@ class UserResource extends JsonResource
             'identify' => $this->id,
             'name' => strtoupper($this->name),
             'email' => $this->email,
-            'created' => $this->created_at,
+            'created' => Carbon::make($this->created_at)->format('Y-m-d'),
         ];
     }
 }
