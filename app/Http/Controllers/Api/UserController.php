520614e6870bb5459527770b9ec6b15b89c036a9
Carlos Ferreira <carlos@especializati.com.br>
2023-03-01 14:47:14 -0300

    feat: create new user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UserResource;
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
@@ -13,5 +14,15 @@ class UserController extends Controller
         $users = User::paginate();
 
         return UserResource::collection($users);
+    }
+
+    public function store(Request $request)
+    {
+        $data = $request->all();
+        $data['password'] = bcrypt($request->password);
+
+        $user = User::create($data);
+
+        return new UserResource($user);
     }
 }
