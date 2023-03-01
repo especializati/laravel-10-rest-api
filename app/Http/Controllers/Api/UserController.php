a2f3909fb29c1b410060e03c075be9b21a2f8235
Carlos Ferreira <carlos@especializati.com.br>
2023-03-01 15:05:31 -0300

    feat: validate store user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateUserRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -16,9 +17,9 @@ class UserController extends Controller
         return UserResource::collection($users);
     }
 
-    public function store(Request $request)
+    public function store(StoreUpdateUserRequest $request)
     {
-        $data = $request->all();
+        $data = $request->validated();
         $data['password'] = bcrypt($request->password);
 
         $user = User::create($data);
---
+++ app/Http/Requests/StoreUpdateUserRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreUpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|min:3|max:255',
+            'email' => [
+                'required',
+                'email',
+                'max:255',
+                'unique:users'
+            ],
+            'password' => [
+                'required',
+                'min:6',
+                'max:100',
+            ]
+        ];
+    }
+}
